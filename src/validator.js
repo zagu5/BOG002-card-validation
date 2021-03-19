adf224474d6b6d65f17d13dcc4025134e3758547
zagu5 <zury.agudelo@gmail.com>
2021-03-19 12:41:05 -0500

    html y css con basse en diseño figma

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
 const validator = {
-  // ...
-};
+  // ...Función que recorre los numeros de la tarjeta de manera inversa 
+  isValid: 
+ }
+
+ /* const creditNumberCardReverse = function (array) {
+    var creditNumberCard = [1,2,3,4,5,6,7,8,9];
+    let newArray = [];
+    let size = array.length;//9
+    let lastPosition = size-1;
+  
+    for ( var i= lastPosition; i>=0; i--){
+    //agrega los elementos del array pero recorriendolo al reves
+    newArray.push(array[i]);
+
+  }
+  return newArray;
+
+}
+}
+  //invoca la funcion numberCardReverse pasando como parámetro numberCard 
+  console.log(creditNumberCard);
+  console.log(creditNumberCardReverse(creditNumberCard));
+*/
+
+
+
 
 export default validator;
